95a722b1767fc88ea43be0bbab192b7ffb468311
Darwin <49041539+dleont@users.noreply.github.com>
2023-08-02 01:06:01 -0500

    update new version jdk 17

--- backend-aws/src/main/java/ec/edu/ups/usuariomicroservicio/Repository/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package ec.edu.ups.usuariomicroservicio.Repository;
-
-import ec.edu.ups.usuariomicroservicio.entity.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PersonaRepository extends JpaRepository<Persona, String> {
-}
